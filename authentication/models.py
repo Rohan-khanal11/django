0e52bebae7b4a74e6ec5911995da828d6b53786e
Rohan Khanal <khanalrohan950@gmail.com>
2025-07-22 10:41:28 +0545

    greece

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+USER=get_user_model()
+class Profile(models.Model):
+     
+    def __str__(self):
+        return self.title
+    
+    
